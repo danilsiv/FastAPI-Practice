a9d41f8b21c80d6a4dd1850b52979929e60f3973
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-07-21 21:40:14 +0300

    Add 'register' endpoint to routers/users.py

--- online_cinema/routers/users.py
+++ online_cinema/routers/users.py
@@ -1,9 +1,21 @@
 from fastapi import APIRouter
+from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.exceptions import HTTPException
 
+from online_cinema.crud import get_user_by_email, create_user
+from online_cinema.database import get_db
+from online_cinema.schemas import UserRead, UserCreate
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-@router.get("/users/")
-async def read_users():
-    return {"message": "List of users"}
+
+@router.post("/register/", response_model=UserRead)
+async def register(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    db_user = await get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return await create_user(db, user)
